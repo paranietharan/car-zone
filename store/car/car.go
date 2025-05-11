ce04f69a98aa1b192562b04a931aa9953d5b1498
Paranietharan <142080100+paranietharan@users.noreply.github.com>
2025-05-11 13:21:44 +0530

    Backup - temp

--- store/car/car.go
+++ store/car/car.go
@@ -1,1 +1,37 @@
 package store
+
+import (
+	"car-zone/models"
+	"context"
+	"database/sql"
+)
+
+type Store struct {
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		db: db,
+	}
+}
+
+func (s Store) GetCarByID(ctx context.Context, id string) (models.Car, error) {
+	//
+}
+
+func (s Store) GetCarByBrand(ctx context.Context, brand string, isEngine bool) ([]models.Car, error) {
+	//
+}
+
+func (s Store) CreateCar(ctx context.Context, car models.Car) (models.Car, error) {
+	//
+}
+
+func (s Store) UpdateCar(ctx context.Context, id string, car models.Car) (models.Car, error) {
+	//
+}
+
+func (s Store) DeleteCar(ctx context.Context, id string) error {
+	//
+}
